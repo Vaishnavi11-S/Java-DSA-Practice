0896df720e6ab05ef31e11003fb2e80e5dace8ec
Vaishnavi11-S <vaishnavishanmugam2006@gmail.com>
2026-08-07 15:50:40 +0530

    Implement MoveZeroes algorithm in MoveZeroes.java

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,27 @@
+public class MoveZeroes {
+    public static void main(String[] args) {
+
+        int[] nums = {0, 1, 0, 3, 12};
+
+        int index = 0;
+
+        // Move non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        // Fill remaining positions with zero
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+
+        // Print array
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
